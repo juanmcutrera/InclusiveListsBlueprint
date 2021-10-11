b6fcfe13167fde847ebfddb2c5de654a0b52cce3
Juan M C <juanmcutrera@gmail.com>
2021-10-11 16:30:51 -0300

    ..

--- app/src/main/java/com/bitropia/inclusivelistsblueprint/data/repositories/ListHeaderLocalDataSource.kt
+++ app/src/main/java/com/bitropia/inclusivelistsblueprint/data/repositories/ListHeaderLocalDataSource.kt
@@ -2,7 +2,7 @@ package com.bitropia.inclusivelistsblueprint.data.repositories
 
 import com.bitropia.inclusivelistsblueprint.domain.entities.ListHeader
 import kotlinx.coroutines.flow.Flow
-
+//
 interface ListHeaderLocalDataSource {
     suspend fun saveListHeader(header: ListHeader): Long
 
