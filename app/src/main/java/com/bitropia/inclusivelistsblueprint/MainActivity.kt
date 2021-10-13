901278ad0dc22673214db4017ce43c9c58acd5b6
Juan M C <juanmcutrera@gmail.com>
2021-10-12 23:52:47 -0300

    .

--- app/src/main/java/com/bitropia/inclusivelistsblueprint/MainActivity.kt
+++ app/src/main/java/com/bitropia/inclusivelistsblueprint/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bitropia.inclusivelistsblueprint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -21,10 +22,21 @@ import androidx.compose.ui.unit.dp
 
 import com.bitropia.inclusivelistsblueprint.theme.InclusiveListsTheme
 import com.bitropia.inclusivelistsblueprint.theme.textField
+import com.bitropia.inclusivelistsblueprint.ui.viewmodels.ListHeaderViewModel
 import dagger.hilt.android.AndroidEntryPoint
-
+import androidx.activity.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    private val viewModel: ListHeaderViewModel by viewModels()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -49,13 +61,29 @@ fun NewsStory() {
         Text("December 2018",color = Color.Yellow)
     }
 }
-
+/*
 @Preview
 @Composable
 fun DefaultPreview() {
     //NewsStory()
     HelloContent()
+}*/
+
+class HelloViewModel : ViewModel() {
+
+    // LiveData holds state which is observed by the UI
+    // (state flows down from ViewModel)
+    private val _name = MutableLiveData("")
+    val name: LiveData<String> = _name
+
+    // onNameChange is an event we're defining that the UI can invoke
+    // (events flow up from UI)
+    fun onNameChange(newName: String) {
+        _name.value = newName
+    }
 }
+
+
 
 @Composable
 fun HelloContent() {
@@ -71,6 +99,7 @@ fun HelloContent() {
             )
         }
     }*/
+
 
     InclusiveListsTheme{
 
@@ -97,7 +126,10 @@ fun HelloContent() {
 
             )*/
             val textState = remember { mutableStateOf(TextFieldValue()) }
+
+
             OutlinedTextField(
+
                 value = textState.value,
                 onValueChange = { textState.value = it },
                 label = { Text("Description") },
@@ -116,3 +148,4 @@ fun HelloContent() {
 
 
 }
+
